26910da2cd3143e86a7d222ce4a1a6fb80bb4ddb
Piotr Zysk <pizy123@gmail.com>
2019-04-16 17:59:12 +0200

    Test with 100 tasks.

--- TPLtest/Program.cs
+++ TPLtest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace TPLtest
 {
@@ -10,6 +12,53 @@ namespace TPLtest
     {
         static void Main(string[] args)
         {
+            /*
+            for (var i=1;i<10;i++)
+            {
+                DoJob(i);
+            }
+            */
+            int NumberOfJobs = 100;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Task[] tasks = new Task[NumberOfJobs];
+
+            for (var i = 1; i <= NumberOfJobs; i++)
+            {
+                var jobNr = i;
+                tasks[i - 1] = Task.Factory.StartNew(() => new Jobs().DoJob(jobNr));
+            }
+
+            Task.WaitAll(tasks);
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            
+
+            Console.WriteLine($"Total job time: {Jobs.MilisecondsCounter} ms. Execution time: {Math.Round(ts.TotalMilliseconds)} ms.");
+
         }
+
     }
+
+    class Jobs
+    { 
+        public static int MilisecondsCounter=0;
+
+        public void DoJob(int jobNr)
+        {
+            var rnd = new Random();
+            var sleepTime = rnd.Next(100);
+            Thread.Sleep(sleepTime);
+            Console.WriteLine($"Job {jobNr} took {sleepTime} miliseconds;");
+            MilisecondsCounter += sleepTime;
+        }
+
+    }
+
+
+    
 }
